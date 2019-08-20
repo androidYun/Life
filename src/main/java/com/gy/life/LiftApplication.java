418f526d16d952e0b271bdadf50007d4512f9cb2
Your Name <xyz_6776@163.com>
2019-08-20 21:57:30 +0800

    改变

--- src/main/java/com/gy/life/LiftApplication.java
+++ src/main/java/com/gy/life/LiftApplication.java
@@ -1,6 +1,5 @@
 package com.gy.life;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
